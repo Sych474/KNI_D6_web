47b89712e9cb45e750e9a8ac5aa98fc3e4a4744c
Святослав Сычев <rk474@yandex.ru>
2019-08-05 18:14:13 +0300

    [KNI-3] add registration

--- KNI_D6_web/Controllers/AccountController.cs
+++ KNI_D6_web/Controllers/AccountController.cs
@@ -2,20 +2,64 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using KNI_D6_web.Model;
+using KNI_D6_web.ViewModels.Account;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace KNI_D6_web.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+
         public IActionResult Login()
         {
             return View();
         }
 
+        [HttpGet]
         public IActionResult Register()
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            IActionResult result = View(model);
+            if (ModelState.IsValid)
+            {
+                User user = new User
+                {
+                    Email = model.Email,
+                    UserName = model.Login,
+                };
+
+                var identityResult = await _userManager.CreateAsync(user, model.Password);
+                if (identityResult.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, false);
+                    result = RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var error in identityResult.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+            return result;
+        }
+
     }
 }
---
+++ KNI_D6_web/ViewModels/Account/RegisterViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KNI_D6_web.ViewModels.Account
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name = "Login")]
+        public string Login { get; set; }
+
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают!")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Подтверждение пароля")]
+        public string PasswordConfirm { get; set; }
+    }
+}
