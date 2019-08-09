9ad8ca1c171b9a9d8aa760e32af4c52aafc7a304
Святослав Сычев <rk474@yandex.ru>
2019-08-09 19:12:29 +0300

    [KNI-5] add db initialization

---
+++ KNI_D6_web.Model/Database/Initialization/Configuration/UserAchievementsPreset.cs
@@ -0,0 +1,9 @@
+﻿namespace KNI_D6_web.Model.Database.Initialization.Configuration
+{
+    public class UserAchievementsPreset
+    {
+        public string UserLogin { get; set; }
+
+        public string AchievementName { get; set; }
+    }
+}
