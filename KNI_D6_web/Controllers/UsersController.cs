77cab8c5abe582b269f95a6916e5c321892ee997
Святослав Сычев <rk474@yandex.ru>
2019-08-09 19:13:37 +0300

    [KNI-5] small update for userDetails

--- KNI_D6_web/Controllers/UsersController.cs
+++ KNI_D6_web/Controllers/UsersController.cs
@@ -2,13 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using KNI_D6_web.Model;
+using KNI_D6_web.Model.Database;
+using KNI_D6_web.ViewModels.Users;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace KNI_D6_web.Controllers
 {
+    [Route("[controller]")]
     public class UsersController : Controller
     {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly SignInManager<User> _signInManager;
+
+        public UsersController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
         // GET: Users
         public ActionResult Index()
         {
@@ -16,9 +31,25 @@ namespace KNI_D6_web.Controllers
         }
 
         // GET: Users/UserDetails/5
-        public ActionResult UserDetails(int id)
+        [Route("{id}")]
+        public IActionResult UserDetails(string id)
         {
-            return View();
+            IActionResult result = BadRequest(id);
+            var _user = _dbContext.Users
+                .Include(x => x.ParameterValues).ThenInclude(pv => pv.Parameter)
+                .Include(x => x.UserAchievements).ThenInclude(pv => pv.Achievement)
+                .Where(u => u.Id == id).FirstOrDefault();
+
+            if (_user != null)
+            {
+                result = View(new UserDetailsViewModel()
+                {
+                    IsAuthorizedUser = User.Identity.IsAuthenticated && (User.Identity.Name == _user.UserName),
+                    User = _user,
+                });
+            }
+
+            return result;
         }
     }
 }
---
+++ KNI_D6_web/ViewModels/Users/UserDetailsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using KNI_D6_web.Model;
+using KNI_D6_web.Model.Parameters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KNI_D6_web.ViewModels.Users
+{
+    public class UserDetailsViewModel
+    {
+        public bool IsAuthorizedUser { get; set; }
+        public User User { get; set; }
+    }
+}
