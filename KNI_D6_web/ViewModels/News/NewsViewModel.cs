a78e441d485d4be8ee6f261495f5e83791688fcd
Святослав Сычев <rk474@yandex.ru>
2019-08-17 00:25:51 +0300

    [KNI-10] add news Pages

---
+++ KNI_D6_web/ViewModels/News/NewsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using KNI_D6_web.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KNI_D6_web.ViewModels.News
+{
+    public class NewsViewModel
+    {
+        public IEnumerable<NewsPost> NewsPosts { get; set; }
+
+        public bool IsAdmin { get; set; }
+    }
+}
