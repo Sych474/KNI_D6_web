75d22236513dafd029cee16f0ce4abfebb113d12
Святослав Сычев <rk474@yandex.ru>
2019-09-02 11:29:34 +0300

    [KNI-24] add email-service

---
+++ KNI_D6_web/Services/EmailConfiguration.cs
@@ -0,0 +1,13 @@
+﻿namespace KNI_D6_web.Services
+{
+    public class EmailConfiguration
+    {
+        public string AdministrationEmail { get; set; }
+
+        public string AdministrationPassword { get; set; }
+        
+        public string AdministrationEmailName { get; set; }
+
+        public string HostEmail { get; set; }
+    }
+}
