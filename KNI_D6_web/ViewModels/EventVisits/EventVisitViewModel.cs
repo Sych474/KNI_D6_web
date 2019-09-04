4cccfe964525ba691c7a367b7b997886307e6c53
Святослав Сычев <rk474@yandex.ru>
2019-09-04 11:16:13 +0300

    [KNI-27] update VisitEvents

---
+++ KNI_D6_web/ViewModels/EventVisits/EventVisitViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace KNI_D6_web.ViewModels.Visits
+{
+    public class EventVisitViewModel
+    {
+        public int EventId { get; set; }
+
+        public string EventName { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public EventVisitState State { get; set; }
+    }
+}
