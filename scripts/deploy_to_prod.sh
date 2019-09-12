1325739e1d105916ef7e24d7535b95d5e469b4b0
Святослав Сычев <rk474@yandex.ru>
2019-09-12 18:21:36 +0300

    #31 update deploy to production

--- scripts/deploy_to_prod.sh
+++ scripts/deploy_to_prod.sh
@@ -1,7 +1,7 @@
+#!/bin/bash
+
 cd ./kni_d6
-if docker-compose up -d; then
-  echo -e "\n[INFO] Stack was successfully deployed."
-else
-    echo -e "\n[ERROR] Couldn't deploy docker stack."
-  exit 4
-fi
+docker-compose pull
+docker-compose stop
+docker-compose rm -f
+docker-compose up -d
