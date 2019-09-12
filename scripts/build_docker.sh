50e5f058994539e6ad160eccc4cfb83c66371562
Святослав Сычев <rk474@yandex.ru>
2019-09-12 20:08:30 +0300

    #31 update

---
+++ scripts/build_docker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+docker login -u=$DOCKER_USERNAME -p=$DOCKER_PASSWORD 
+sed -i "s/\"AdministrationPassword\": \"qwerty\",/\"AdministrationPassword\": \"$EMAIL_PASSWORD\",/g" ./KNI_D6_web/appsettings.json
+docker build -t sych474/$SERVICE_NAME:latest ./
+docker push sych474/$SERVICE_NAME:latest
+      
