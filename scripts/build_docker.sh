d1323210030830ef0bd5d0d70e00730aec743d35
Святослав Сычев <rk474@yandex.ru>
2019-09-12 20:30:18 +0300

    #31 end test

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -2,7 +2,6 @@
 
 docker login -u=$DOCKER_USERNAME -p=$DOCKER_PASSWORD 
 sed -i "s/\"AdministrationPassword\": \"qwerty\",/\"AdministrationPassword\": \"$EMAIL_PASSWORD\",/g" ./KNI_D6_web/appsettings.json
-cat ./KNI_D6_web/appsettings.json
 docker build -t sych474/$SERVICE_NAME:latest ./
 docker push sych474/$SERVICE_NAME:latest
       
