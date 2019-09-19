3b7e7cdba3682467f12fd002f056e5b1d33a5ebe
Святослав Сычев <rk474@yandex.ru>
2019-09-20 00:54:35 +0300

    #44 add achievements progress componenet

---
+++ KNI_D6_web/ViewModels/Components/AchievementsProgress/AchievementProgressViewModel.cs
@@ -0,0 +1,23 @@
+﻿using KNI_D6_web.Model.Achievements;
+
+namespace KNI_D6_web.ViewModels.Components.AchievementsProgress
+{
+    public class AchievementProgressViewModel
+    {
+        public int AchievementId { get; set; }
+
+        public string AchievementName { get; set; }
+
+        public string AchievementDescription { get; set; }
+
+        public int? AchievementValue { get; set; }
+
+        public int NumberInGroup { get; set; }
+
+        public int? LinkedParameterValue { get; set; }
+
+        public bool IsReceived { get; set; }
+
+        public AchievementType AchievementType { get; set; }
+    }
+}
---
+++ KNI_D6_web/ViewModels/Components/AchievementsProgress/AchievementsProgressViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace KNI_D6_web.ViewModels.Components.AchievementsProgress
+{
+    public class AchievementsProgressViewModel
+    {
+        public IEnumerable<IOrderedEnumerable<AchievementProgressViewModel>> AchievementsProgressInGroups { get; set; }
+    }
+}
