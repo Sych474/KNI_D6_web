72dcabb3aa45b3abdb4a1c2841ff33f91eaf54a3
Святослав Сычев <rk474@yandex.ru>
2019-09-23 22:33:33 +0300

    #54 add achievementsGroups repository

---
+++ KNI_D6_web.Model/Database/Repositories/IAchievementsGroupsRepository.cs
@@ -0,0 +1,21 @@
+﻿using KNI_D6_web.Model.Achievements;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KNI_D6_web.Model.Database.Repositories
+{
+    public interface IAchievementsGroupsRepository
+    {
+        Task<List<AchievementsGroup>> GetAchievementsGroupsAsync();
+
+        Task<int> AddAchievementsGroupAsync(AchievementsGroup entity);
+
+        Task UpdateAchievementsGroupAsync(AchievementsGroup entity);
+
+        Task RemoveAchievementsGroupByIdAsync(int id);
+
+        Task<AchievementsGroup> FindAchievementsGroupByIdAsync(int id);
+    }
+}
---
+++ KNI_D6_web.Model/Database/Repositories/Implementation/AchievementsGroupsRepository.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using KNI_D6_web.Model.Achievements;
+using Microsoft.EntityFrameworkCore;
+
+namespace KNI_D6_web.Model.Database.Repositories.Implementation
+{
+    public class AchievementsGroupsRepository : BaseRepository, IAchievementsGroupsRepository
+    {
+        public AchievementsGroupsRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        public async Task<int> AddAchievementsGroupAsync(AchievementsGroup entity)
+        {
+            context.AchievementsGroups.Add(entity);
+            await context.SaveChangesAsync();
+            return entity.Id;
+        }
+
+        public Task<AchievementsGroup> FindAchievementsGroupByIdAsync(int id)
+        {
+            return context.AchievementsGroups.FirstOrDefaultAsync(a => a.Id == id);
+        }
+
+        public Task<List<AchievementsGroup>> GetAchievementsGroupsAsync()
+        {
+            return context.AchievementsGroups?.Include(ag => ag.Ahievements)?.ThenInclude(a => a.Parameter)?.ToListAsync();
+        }
+
+        public async Task RemoveAchievementsGroupByIdAsync(int id)
+        {
+            var entity = await context.AchievementsGroups.FirstOrDefaultAsync(a => a.Id == id);
+            if (entity == null)
+            {
+                context.AchievementsGroups.Remove(entity);
+                await context.SaveChangesAsync();
+            }
+        }
+
+        public async Task UpdateAchievementsGroupAsync(AchievementsGroup entity)
+        {
+            context.AchievementsGroups.Update(entity);
+            await context.SaveChangesAsync();
+        }
+    }
+}
