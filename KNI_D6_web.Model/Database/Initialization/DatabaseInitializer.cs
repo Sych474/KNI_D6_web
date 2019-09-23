40362a970733d543862969b43873615f43cd716f
Святослав Сычев <rk474@yandex.ru>
2019-09-23 19:41:45 +0300

    #51 update dbInitialization

--- KNI_D6_web.Model/Database/Initialization/DatabaseInitializer.cs
+++ KNI_D6_web.Model/Database/Initialization/DatabaseInitializer.cs
@@ -1,35 +1,55 @@
-﻿using KNI_D6_web.Model.Achievements;
-using KNI_D6_web.Model.Database.Initialization.Configuration;
+﻿using KNI_D6_web.Model.Database.Initialization.Configuration;
+using KNI_D6_web.Model.Database.Repositories;
 using KNI_D6_web.Model.Parameters;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace KNI_D6_web.Model.Database.Initialization
 {
     public class DatabaseInitializer
     {
-        public static async Task InitializeDatabase(ApplicationDbContext dbContext, UserManager<User> userManager, RoleManager<IdentityRole> roleManager, DbInitializationConfiguration configuration)
+        private readonly UserManager<User> userManager;
+        private readonly RoleManager<IdentityRole> roleManager;
+        private readonly IParametersRepository parametersRepository;
+        private readonly IParameterValuesRepository parameterValuesRepository;
+
+        public DatabaseInitializer(UserManager<User> userManager, RoleManager<IdentityRole> roleManager, IParametersRepository parametersRepository, IParameterValuesRepository parameterValuesRepository)
+        {
+            this.userManager = userManager;
+            this.roleManager = roleManager;
+            this.parametersRepository = parametersRepository;
+            this.parameterValuesRepository = parameterValuesRepository;
+        }
+
+        public async Task InitializeDatabase(DbInitializationConfiguration configuration)
         {
             await AddUserRoles(roleManager);
 
-            await AddParameters(dbContext, configuration.Parameters);
+            await AddParameters(configuration.Parameters);
 
-            await AddAdminUser(userManager, configuration.AdminLogin, configuration.AdminPassword, configuration.AdminEmail);
+            await AddAdminUser(configuration.AdminLogin, configuration.AdminPassword, configuration.AdminEmail);
         }
 
-        private static async Task AddAdminUser(UserManager<User> userManager, string adminLogin, string adminPassword, string adminEmail)
+        private async Task AddAdminUser(string adminLogin, string adminPassword, string adminEmail)
         {
             var admin = await userManager.FindByNameAsync(adminLogin);
             if (admin == null)
             {
-                admin = new User() { UserName = adminLogin, Email = adminEmail };
+                admin = new User()
+                {
+                    UserName = adminLogin,
+                    Email = adminEmail
+                };
                 var identityResult = await userManager.CreateAsync(admin, adminPassword);
+
                 if (!identityResult.Succeeded)
                     throw new Exception("Can not create admin user");
+
+                var createdAdmin = await userManager.FindByNameAsync(adminLogin);
+
+                await parameterValuesRepository.AddAllParameterValuesForUserAsync(createdAdmin.Id);
             }
 
             foreach (var role in UserRoles.Roles)
@@ -39,7 +59,7 @@ namespace KNI_D6_web.Model.Database.Initialization
             }
         }
 
-        private static async Task AddUserRoles(RoleManager<IdentityRole> roleManager)
+        private async Task AddUserRoles(RoleManager<IdentityRole> roleManager)
         {
             foreach (var item in UserRoles.Roles)
             {
@@ -48,52 +68,24 @@ namespace KNI_D6_web.Model.Database.Initialization
             }
         }
 
-        private static async Task AddParameters(ApplicationDbContext dbContext, IEnumerable<string> parameterNames)
+        private async Task AddParameters(IEnumerable<string> parameterNames)
         {
             foreach (var name in parameterNames)
             {
-                if (!await dbContext.Parameters.AnyAsync(x => x.Name == name))
+                var currParameter = await parametersRepository.FindParameterByNameAsync(name);
+                if (currParameter == null)
                 {
-                    await dbContext.Parameters.AddAsync(new Parameter() { Name = name });
-                    await dbContext.SaveChangesAsync();
-                }
-            }
-        }
-
-        private static async Task AddParameterValues(ApplicationDbContext dbContext, IEnumerable<Parameter> parameters, IEnumerable<ParameterValuesPreset> parameterValuesPresets)
-        {
-            foreach (var item in parameterValuesPresets)
-            {
-                var parameter = dbContext.Parameters.AsNoTracking().Where(p => p.Name == item.ParameterName).FirstOrDefault();
-                var user = dbContext.Users.AsNoTracking().Where(u => u.UserName == item.UserLogin).FirstOrDefault();
-                if (parameter != null && user != null)
-                {
-                    var parameterValue = dbContext.ParameterValues.Where(x => x.UserId == user.Id && x.ParameterId == parameter.Id).FirstOrDefault();
-                    if (parameterValue == null)
-                        await dbContext.ParameterValues.AddAsync(new ParameterValue() { UserId = user.Id, ParameterId = parameter.Id, Value = item.Value });
-                    else if (parameterValue.Value == ParameterValue.DefaultValue)
-                        parameterValue.Value = item.Value;                        
-                    await dbContext.SaveChangesAsync();
-                }
-            }
-
-            //add Default values
-            /*foreach (var user in dbContext.Users.Include(u=>u.ParameterValues).ThenInclude(pv => pv.Parameter))
-            {
-                foreach (var name in parameterNames)
-                {
-                    var parameter = dbContext.Parameters.AsNoTracking().Where(p => p.Name == name).FirstOrDefault();
-                    if (parameter != null)
+                    try
                     {
-                        if (!await dbContext.ParameterValues.AnyAsync(x => x.UserId == user.Id && x.ParameterId == parameter.Id))
-                        {
-                            await dbContext.ParameterValues.AddAsync(new ParameterValue() { UserId = user.Id, ParameterId = parameter.Id, Value = DefaultValue });
-                            await dbContext.SaveChangesAsync();
-                        }
+                        await parametersRepository.AddParameterAsync(new Parameter() { Name = name });
+                    }
+                    catch (Exception ex)
+                    {
+                        //TO_DO log errors
+                        Console.WriteLine(ex.Message);
                     }
                 }
             }
-            */
         }
     }
 }
