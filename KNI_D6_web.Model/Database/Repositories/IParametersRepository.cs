fcefc7bc64b77700f38f6daab601bc0f2c7f0553
Святослав Сычев <rk474@yandex.ru>
2019-09-23 17:50:11 +0300

    #51 add first version of parametersRepository

---
+++ KNI_D6_web.Model/Database/Repositories/IParametersRepository.cs
@@ -0,0 +1,17 @@
+﻿using KNI_D6_web.Model.Parameters;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace KNI_D6_web.Model.Database.Repositories
+{
+    public interface IParametersRepository 
+    {
+        Task<bool> AddParameterAsync(Parameter entity);
+
+        Task<bool> UpdateParameterAsync(Parameter entity);
+
+        Task<bool> RemoveParameterByIdAsync(int id);
+
+        Task<List<Parameter>> GetParametersAsync();
+    }
+}
---
+++ KNI_D6_web.Model/Database/Repositories/Implementation/ParametersRepository.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using KNI_D6_web.Model.Parameters;
+using Microsoft.EntityFrameworkCore;
+
+namespace KNI_D6_web.Model.Database.Repositories.Implementation
+{
+    public class ParametersRepository : BaseRepository, IParametersRepository
+    {
+        public ParametersRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        public async Task<bool> AddParameterAsync(Parameter entity)
+        {
+            bool result = true;
+            try
+            {
+                context.Parameters.Add(entity);
+                await context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                result = false;
+            }
+
+            return result;
+        }
+
+        public Task<List<Parameter>> GetParametersAsync()
+        {
+            return context.Parameters.ToListAsync();
+        }
+
+        public async Task<bool> RemoveParameterByIdAsync(int id)
+        {
+            bool result = true;
+            try
+            {
+                var parameter = await context.Parameters.FirstOrDefaultAsync(p => p.Id == id);
+                if (parameter != null)
+                {
+                    context.Parameters.Remove(parameter);
+                    await context.SaveChangesAsync();
+                }
+            }
+            catch (DbUpdateException)
+            {
+                result = false;
+            }
+
+            return result;
+        }
+
+        public async Task<bool> UpdateParameterAsync(Parameter entity)
+        {
+            bool result = true;
+            try
+            {
+                context.Parameters.Update(entity);
+                await context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                result = false;
+            }
+
+            return result;
+        }
+    }
+}
