8f9e14330099dfecd56af0ded6023d52c0de441f
Святослав Сычев <rk474@yandex.ru>
2019-09-26 20:21:21 +0300

    #56 fix password and login inpiut

--- KNI_D6_web/ViewModels/Account/ChangePasswordViewModel.cs
+++ KNI_D6_web/ViewModels/Account/ChangePasswordViewModel.cs
@@ -8,17 +8,17 @@ namespace KNI_D6_web.ViewModels.Account
         public string Login { get; set; }
 
         [Required(ErrorMessage = "Поле должно быть заполнено")]
-        [StringLength(100, ErrorMessage = "Пароль должен иметь не менее 5 символов", MinimumLength = 5)]
+        [StringLength(DefaultAuthorizetionSettings.PasswordMaxLength, ErrorMessage = DefaultAuthorizetionSettings.PasswordErrorMessage, MinimumLength = DefaultAuthorizetionSettings.PasswordMinLength)]
         [DataType(DataType.Password)]
         public string OldPassword { get; set; }
 
         [Required(ErrorMessage = "Поле должно быть заполнено")]
-        [StringLength(100, ErrorMessage = "Пароль должен иметь не менее 5 символов", MinimumLength = 5)]
+        [StringLength(DefaultAuthorizetionSettings.PasswordMaxLength, ErrorMessage = DefaultAuthorizetionSettings.PasswordErrorMessage, MinimumLength = DefaultAuthorizetionSettings.PasswordMinLength)]
         [DataType(DataType.Password)]
         public string NewPassword { get; set; }
 
         [Required(ErrorMessage = "Поле должно быть заполнено")]
-        [StringLength(100, ErrorMessage = "Пароль должен иметь не менее 5 символов", MinimumLength = 5)]
+        [StringLength(DefaultAuthorizetionSettings.PasswordMaxLength, ErrorMessage = DefaultAuthorizetionSettings.PasswordErrorMessage, MinimumLength = DefaultAuthorizetionSettings.PasswordMinLength)]
         [Compare("NewPassword", ErrorMessage = "Пароли не совпадают!")]
         [DataType(DataType.Password)]
         public string NewPasswordConfirm { get; set; }
